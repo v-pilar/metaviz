d4a5b592026da3be63c8f0d1a26237f722847036
v-pilar <verena.pilar@univie.ac.at>
2026-02-10 00:50:23 +0100

    add wineqtlma extension

---
+++ R/wineq_plots.R
@@ -0,0 +1,14 @@
+
+#'@title Weight inequality plots: A group of plots developed for the comparison
+#'of fixed-effect and random-effects models
+
+
+
+#'@description The weight inequality, i.e. wineq plots, are a group of plots
+#'developed for the visual comparison of fixed-effect and random effects models.
+#'Using both models in tandem allows for a sample-specific as well as a generalizable
+#'analysis of the data. The wineq plot functions wineq_forest, wineq_gini and
+#'wineq_baujat provide means to compare overall effects, study weights, weight
+#'concentration, and influence of a study on the overall effect, respectively,
+#'between the fixed-effect and random-effects model, offering ample opportunity
+#'to investigate and interpret the sample.
